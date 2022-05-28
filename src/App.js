9c5160ac7256747bdc27c5a670ece1dedf80d515
YeremiaJoy <yeremiajoy@gmail.com>
2022-05-28 10:47:23 +0700

    step 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import initData from "./data.json";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  function getData() {
+    let arr = [];
+    initData.data.forEach((val) => {      
+      let obj = {};
+      //declare key of new object
+      obj["id"] = val.id;
+      obj["full_name"] = `${val.first_name} ${val.last_name}`;
+      obj["expert_skills"] = [];
+      //split skill to [key,value]
+      const skill = Object.entries(val.skills);
+      skill.forEach(([key, val]) => {
+        if (val === "expert") obj["expert_skills"].push(key);
+      });
+      arr.push(obj);
+    });
+    setData(arr);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(data)}
     </div>
   );
 }
